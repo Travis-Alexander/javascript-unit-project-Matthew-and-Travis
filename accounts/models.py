3ddb0ddc99506495009b51d3937f436b03d8e74f
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-04-19 15:54:01 -0500

    Set up user model and card model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from cards.models import Cards
 # Create your models here.
+class CustomUser(AbstractUser):
+    wins = models.PositiveIntegerField(default=0)
+    losses = models.PositiveIntegerField(default=0)
+    decks = models.ManyToManyField(Cards, blank=True, symmetrical=False, related_name="decks")
+
+
--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Cards(models.Model):
+    attack = models.PositiveIntegerField()
+    health = models.PositiveIntegerField()
+    defense = models.PositiveIntegerField()
+    card_art = models.CharField(max_length=150)
+    effects = models.CharField(max_length=150)
+    level = models.PositiveIntegerField()
