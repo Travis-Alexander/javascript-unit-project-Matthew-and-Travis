b0c8afde2df87edf8438dd505c993721ede1489e
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2021-04-20 14:29:26 -0500

    friend request and removing friends work

--- accounts/models.py
+++ accounts/models.py
@@ -6,5 +6,15 @@ class CustomUser(AbstractUser):
     wins = models.PositiveIntegerField(default=0)
     losses = models.PositiveIntegerField(default=0)
     decks = models.ManyToManyField(Card, blank=True, symmetrical=False, related_name="decks")
+    friends = models.ManyToManyField("CustomUser", blank=True)
+
+    def __str__(self):
+        return self.username
 
 
+class FriendRequest(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="user")
+    receiver = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="receiver")
+
+    def __str__(self):
+        return f"Sender: {self.user}, Receiver: {self.receiver}"
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect, HttpResponse
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser, FriendRequest
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,3 +13,57 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+class UserProfile(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = "users/user_profile.html"
+    context_object_name = "profile_user"
+
+
+
+def SendFriendRequest(request, userID):
+    user = request.user
+    receiver = CustomUser.objects.get(id=userID)
+    friendRequest, created = FriendRequest.objects.get_or_create(
+        user=user, receiver=receiver
+    )
+    if created:
+        messages.success(request, "Friend request sent.")
+        print("sent")
+        return redirect(f"/user/{receiver.id}")
+    else:
+        messages.error(request, "Friend request already created.")
+        print('stop')
+        return redirect(f"/user/{receiver.id}")
+
+
+def AcceptFriendRequest(request, requestID):
+    friendRequest = FriendRequest.objects.get(id=requestID)
+    if friendRequest.receiver == request.user:
+        friendRequest.user.friends.add(friendRequest.receiver)
+        friendRequest.receiver.friends.add(friendRequest.user)
+        friendRequest.delete()
+        messages.success(request, "Friend request accepted.")
+        return redirect(f"/user/{request.user.id}")
+    else:
+        messages.error(request, "Friend request not accepted.")
+        return redirect(f"/user/{request.user.id}")
+
+def DeclineFriendRequest(request, requestID):
+    friendRequest = FriendRequest.objects.get(id=requestID)
+    if friendRequest.receiver == request.user:
+        friendRequest.delete()
+        messages.success(request, "Friend request declined.")
+        return redirect(f"/user/{request.user.id}")
+    else:
+        messages.error(request, "Friend request not declined.")
+        return redirect(f"/user/{request.user.id}")
+
+
+def RemoveFriend(request, userID):
+    user = request.user
+    friend = CustomUser.objects.get(id=userID)
+    user.friends.remove(friend)
+    friend.friends.remove(user)
+    messages.success(request, "Friend removed.")
+    return redirect(f"/user/{request.user.id}")
